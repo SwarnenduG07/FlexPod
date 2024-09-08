1e84d7bc7bb5a8f37e9347aeba3839bc361308b0
Sunny <swarnendughosh05@gmail.com>
2024-09-09 01:03:14 +0530

    appbar added

--- app/components/Appbar.tsx
+++ app/components/Appbar.tsx
@@ -1,15 +1,22 @@
 "use client"
 
+import { Button } from '@/components/ui/button'
 import { useRouter } from 'next/navigation'
 import React from 'react'
 
 export const Appbar = () => {
     const router = useRouter()
   return (
-    <div>
-          <button onClick={() => {
-            router.push("api/auth/signin")
-          }}>Signin</button>
+    <div className='py-4 border-b border-purple-700 flex flex-wrap justify-between'>
+        <div className='lg:text-2xl md:text-2xl text-xl ml-14 font-bold'>
+            FluxPod 
+        </div>
+        <div className='mr-10'>
+            <Button className='w-24 h-10 bg-gradient-to-bl from-purple-600 to-neutral-900 text-white  text-sm' 
+            onClick={() => {
+                router.push("/api/auth/signin")
+            }}>Signin</Button>
+        </div>
     </div>
   )
 }
--- app/page.tsx
+++ app/page.tsx
@@ -3,7 +3,7 @@ import { Appbar } from "./components/Appbar";
 
 export default function Home() {
   return (
-     <div className="text-emerald-400 flex flex-col items-center justify-center h-screen">
+     <div>
          <Appbar />
      </div>
   );
