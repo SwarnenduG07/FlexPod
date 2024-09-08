868e25c90b92b27366b4c265c2405926947c1cb6
Sunny <swarnendughosh05@gmail.com>
2024-09-09 01:42:01 +0530

    auth adde

--- app/api/auth/[...nextauth]/route.ts
+++ app/api/auth/[...nextauth]/route.ts
@@ -1,30 +1,67 @@
-import NextAuth from "next-auth"
-import CredentialsProvider from 'next-auth/providers/credentials';
+import CredentialsProvider from "next-auth/providers/credentials"
 import bcrypt from "bcrypt";
+import { PrismaClient } from "@prisma/client/extension";
+const client = new PrismaClient(); 
 
-const handler = NextAuth({
-  providers: [
-    CredentialsProvider({
-        name: 'Credentials',
-        credentials: {
-          username: { label: 'email', type: 'text', placeholder: 'email' },
-          password: { label: 'password', type: 'password', placeholder: 'password' },
-          OTP: { label: 'OTP', type: 'OTP', placeholder: 'Enter your Otp' },
-        },
-        async authorize(credentials) {
-            const hashedPassword = await bcrypt.hash(credentials?.password, 10);
-            const existingUser = await db.USer.findFirst({
-                
-            })
-            console.log(credentials);
-             
-            return {
-                id: "user1"
-            };
-        },
-      })
-  ],
-  secret: process.env.NEXTAUTH_SECRET
-})
+export const authOptions = {
+    providers: [
+      CredentialsProvider({
+          name: 'Credentials',
+          credentials: {
+            phone: { label: "Phone number", type: "text", placeholder: "1231231231", required: true },
+            password: { label: "Password", type: "password", required: true }
+          },
+          // TODO: User credentials type from next-aut
+          async authorize(credentials: any) {
+            // Do zod validation, OTP validation here
+            const hashedPassword = await bcrypt.hash(credentials.password, 10);
+            const existingUser = await client.user.findFirst({
+                where: {
+                    number: credentials.phone
+                }
+            });
 
-export { handler as GET, handler as POST }
+            if (existingUser) {
+                const passwordValidation = await bcrypt.compare(credentials.password, existingUser.password);
+                if (passwordValidation) {
+                    return {
+                        id: existingUser.id.toString(),
+                        name: existingUser.name,
+                        email: existingUser.number
+                    }
+                }
+                return null;
+            }
+
+            try {
+                const user = await client.user.create({
+                    data: {
+                        number: credentials.phone,
+                        password: hashedPassword
+                    }
+                });
+            
+                return {
+                    id: user.id.toString(),
+                    name: user.name,
+                    email: user.number
+                }
+            } catch(e) {
+                console.error(e);
+            }
+
+            return null
+          },
+        })
+    ],
+    secret: process.env.JWT_SECRET || "secret",
+    callbacks: {
+        // TODO: can u fix the type here? Using any is bad
+        async session({ token, session }: any) {
+            session.user.id = token.sub
+
+            return session
+        }
+    }
+  }
+  
